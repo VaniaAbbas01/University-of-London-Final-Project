9beeecbfbe30b771c5160ee571810d562ab9cb71
Vania Abbas <vaniaabbas2@gmail.com>
2025-05-08 14:59:09 +0500

    added code for file upload

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_secret_key_here'
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class uploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Submit")
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    form = uploadFileForm()
+    return render_template("index.html", form=form)
 
 @app.route("/name")
 def hello_name():
+
     return "<p>Hello, Name!</p>"
 
 if __name__ == "__main__":
