2c7d91f8eb3fcd7310bbd5da807ac210eae7b20f
Vania Abbas <vaniaabbas2@gmail.com>
2025-05-09 13:29:32 +0500

    wrote code for input validation

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key_here'
 
-class uploadFileForm(FlaskForm):
-    file = FileField("File")
-    submit = SubmitField("Submit")
-
 @app.route("/", methods=["GET", "POST"])
-@app.route("/home", methods=["GET", "POST"])
-def home():
-    form = uploadFileForm()
-    return render_template("index.html", form=form)
+def index():
+    return render_template("index.html")
 
-@app.route("/name")
-def hello_name():
+ALLOWED_EXTENSIONS = {'mp4'}
 
-    return "<p>Hello, Name!</p>"
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if 'video' not in request.files:
+        return "No file part", 400
+    video = request.files['video']
+    if video.filename == '':
+        return "No selected file", 400
+    if video and allowed_file(video.filename):
+        video.save('static/uploads/' + video.filename)
+    return "invalid file type", 400
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
