4f394185a985039cb6208419a363f2b0b42969b1
Vania Abbas <vaniaabbas2@gmail.com>
2025-05-22 16:10:14 +0500

    added code for webcam capture

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import cv2
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key_here'
+camera = cv2.VideoCapture(0)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -11,6 +13,21 @@ ALLOWED_EXTENSIONS = {'mp4'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def generateFrames():
+    while True:
+        ## read the camera frame
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    
 
 @app.route("/upload", methods=["POST"])
 def upload():
@@ -22,6 +39,10 @@ def upload():
     if video and allowed_file(video.filename):
         video.save('static/uploads/' + video.filename)
     return "invalid file type", 400
+
+@app.route("/video")
+def video():
+    return Response(generateFrames(), mimetype="multipart/x-mixed-replace; boundary=frame")
     
 
 
