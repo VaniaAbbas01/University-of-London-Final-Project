8b3d0593d78f436e737c8265b00b223b2b8afade
Vania Abbas <vaniaabbas2@gmail.com>
2025-07-07 13:33:31 +0500

    averaged the WER metric

--- modelTests/Speech-To-Text/speech-to-text.py
+++ modelTests/Speech-To-Text/speech-to-text.py
@@ -7,7 +7,7 @@ from faster_whisper import WhisperModel
 from jiwer import wer
 
 # CSV path 
-CSV_PATH = "./audioFiles/audio.tsv"
+CSV_PATH = "./audioFiles/audio.csv"
 # # Models to test
 MODEL_SIZES = ["tiny", "base", "small"]
 
@@ -17,58 +17,59 @@ results = []
 df = pd.read_csv(CSV_PATH)
 reference_text = []
 
-print(df.columns)
-
-print(df.head())
-print(df["text"])
-
 for value in df["text"]:
     reference_text.append(value.strip().lower())
 
 i = 0
-for value in df['filename']:
+for size in MODEL_SIZES:
     print("Benchmarking OpenAI Whisper...")
-    for size in MODEL_SIZES:
+    sum_wer = 0
+    for value in df['filename']:
         model = whisper.load_model(size)
-        model.transcribe(value)
+        model.transcribe("./audioFiles/"+ value)
         start = time.time()
-        result = model.transcribe(value) 
+        result = model.transcribe("./audioFiles/"+ value) 
         duration = time.time() - start
         output_text = result["text"].strip().lower()
         error = wer(reference_text[i], output_text)
-        results.append([f"Whisper ({size})",f"{reference_text[i]}" ,f"{duration:.2f}",f"{error:.3f}", output_text[:60] + "..."])
+        sum_wer += error
+    avg_wer = sum_wer / len(df['filename'])
+    results.append([f"Whisper ({size})" ,f"{duration:.2f}",f"{avg_wer:.3f}"])
     if i <= len(reference_text) - 1:
         i = i + 1
 
 i = 0
-for value in df['filename']:
-    print("Benchmarking OpenAI Whisper...")
-    for size in MODEL_SIZES:
+
+for size in MODEL_SIZES:
+    sum_wer = 0
+    for value in df['filename']:
+        print("Benchmarking OpenAI Whisper...")
         model = WhisperModel(size, device="cpu", compute_type="int8")
-        list(model.transcribe(value))
+        list(model.transcribe("./audioFiles/"+ value))
 
         start = time.time()
-        segments, _ = model.transcribe(value)
+        segments, _ = model.transcribe("./audioFiles/"+ value)
         segments = list(segments)  
         duration = time.time() - start
         text = " ".join([seg.text for seg in segments])
         output_text = " ".join([seg.text for seg in segments]).strip().lower()
         print(output_text)
         error = wer(reference_text[i], output_text)
-
-        results.append([f"Faster-Whisper ({size})", f"{reference_text[i]}", f"{duration:.2f}", f"{error:.3f}", output_text[:60] + "..."])
+        sum_wer += error
+    avg_wer = sum_wer / len(df['filename'])
+    results.append([f"Faster-Whisper ({size})", f"{duration:.2f}", f"{avg_wer:.3f}"])
     if i <= len(reference_text) - 1:
         i = i + 1
         
 # Print table
 print("\nTranscription Time Comparison:")
-print(tabulate(results, headers=["Model", "Inference Time (s)", "WER", "Sample Output", "Word Error Rate"]))
+print(tabulate(results, headers=["Model", "Inference Time (s)", "WER", "Word Error Rate"]))
 
 # Save to CSV
 csv_path = "speech-to-text.csv"
 with open(csv_path, mode="w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(["Model", "Original Text", "Inference Time (s)", "WER", "Sample Output"])
+    writer.writerow(["Model", "Inference Time (s)", "WER"])
     writer.writerows(results)
 
 print(f"\nResults saved to: {csv_path}")
