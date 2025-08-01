bd027dba54f1bc266ed8d8900be34168b48c3f1e
Vania Abbas <vaniaabbas2@gmail.com>
2025-08-01 16:50:41 +0500

    changed the code to OOP

--- models/speech_to_text.py
+++ models/speech_to_text.py
@@ -1,15 +1,18 @@
 import whisper
 
+class Transcription:
+    def __init__(self):
+        self.model = self.loadModel()
 
-def loadModel():
-    """Load the OpenAI Whisper model."""
-    print("Loading OpenAI Whisper model...")
-    return whisper.load_model("small")  
+    # load the OpenAI Whisper model
+    def loadModel(self):
+        """Load the OpenAI Whisper model."""
+        return whisper.load_model("small")
 
-
-def transcribeAudio(model, audio_path):
-    """Transcribe audio using the OpenAI Whisper model."""
-    print(f"Transcribing audio file: {audio_path}")
-    result = model.transcribe(audio_path)
-    return result["text"].strip().lower()
+    # Transcribe audio using the OpenAI Whisper model
+    def transcribeAudio(self, audio_path):
+        """Transcribe audio using the OpenAI Whisper model."""
+        print(f"Transcribing audio file: {audio_path}")
+        result = self.model.transcribe(audio_path)
+        return result["text"].strip().lower()
 
