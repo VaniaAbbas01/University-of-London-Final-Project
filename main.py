a802f42c3fb8fde3800603378136f540e653f3e0
Vania Abbas <vaniaabbas2@gmail.com>
2025-09-07 13:12:32 +0500

    created a create_app() function and moved the blueprints and config in there

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from routes.mainRoute import main_bp
 from routes.uploadRoute import upload_bp
 from routes.analysisRoute import analysis_bp
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-# Register Blueprints
-app.register_blueprint(main_bp)
-app.register_blueprint(upload_bp)
-app.register_blueprint(analysis_bp)
+    # Register Blueprints
+    app.register_blueprint(main_bp)
+    app.register_blueprint(upload_bp)
+    app.register_blueprint(analysis_bp)
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
